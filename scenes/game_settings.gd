00c78dcd78df9a03037605b07910c16a1a566c04
Shining Wing <shiningwing@pm.me>
2024-01-15 00:53:50 -0400

    Add config version variable

--- scenes/game_settings.gd
+++ scenes/game_settings.gd
@@ -8,6 +8,9 @@ var general_settings = ConfigFile.new()
 var general_settings_load = general_settings.load("user://cfg/general.cfg")
 
 # Start defining settings here
+# Config version, use this in all of them
+var config_version: int = 3
+
 # Input Settings
 # Mouselook
 var mouse_sensitivity_x: float = 10.0
@@ -19,6 +22,10 @@ var gyro_sensitivity_y: float = 2.0
 
 # General
 var debug_mode := true
+
+# Graphics
+var resolution_w: int = 1280
+var resoultion_h: int = 720
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,6 +41,7 @@ func _ready():
 	if input_settings_load != OK:
 		# Initialize input settings
 		print("Input settings config doesn't exist, creating...")
+		input_settings.set_value("Meta", "config_version", config_version)
 		input_settings.set_value("Mouselook", "mouse_sensitivity_x", mouse_sensitivity_x)
 		input_settings.set_value("Mouselook", "mouse_sensivitity_y", mouse_sensitivity_y)
 		input_settings.set_value("Gyro", "gyro_enabled", gyro_enabled)
@@ -52,6 +60,7 @@ func _ready():
 	# Load general settings
 	if general_settings_load != OK:
 		print("General settings config doesn't exist, creating...")
+		general_settings.set_value("Meta", "config_version", config_version)
 		general_settings.set_value("Debug", "debug_mode", debug_mode)
 		general_settings.save("user://cfg/general.cfg")
 		print("General settings config created!")
