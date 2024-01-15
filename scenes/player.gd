6f6f62061a927b984153868f7f382536a5fe86df
Shining Wing <shiningwing@pm.me>
2024-01-14 20:21:45 -0400

    Add mouselook FPS controls

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,15 +1,30 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED: float = 4.0
 const JUMP_VELOCITY = 4.5
+
+@export var min_look_angle: float = -90.0
+@export var max_look_angle: float = 90.0
+@export var mouse_sensitivity_x: float = 10.0
+@export var mouse_sensitivity_y: float = 10.0
+
+var mouse_delta: Vector2 = Vector2()
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var camera: Camera3D = $Camera3D
+@onready var camera_x_angle_label: Label = $HUD0/CameraXAngleLabel
+@onready var camera_y_angle_label: Label = $HUD0/CameraYAngleLabel
+
+
+func _ready():
+	# Lock mouse cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _physics_process(delta):
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -19,7 +34,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector(
 			"player_strafeleft",
 			"player_straferight",
@@ -34,3 +48,34 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _process(delta):
+	# Apply mouselook rotation
+	process_mouse_input(delta)
+	
+	# Exit game when Esc is pressed
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+
+
+func _input(event):
+	# Get the mouse delta for mouselook
+	if event is InputEventMouseMotion:
+		mouse_delta = event.relative
+
+
+func process_mouse_input(delta):
+	# Rotate camera around X axis
+	camera.rotation_degrees.x -= mouse_delta.y * mouse_sensitivity_y * delta
+	# Clamp camera X rotation
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, min_look_angle, max_look_angle)
+	# Rotate player around Y axis
+	rotation_degrees.y -= mouse_delta.x * mouse_sensitivity_x * delta
+	
+	# Zero out mouse delta to avoid input "floating"
+	mouse_delta = Vector2.ZERO
+	
+	# Update camera angles in HUD
+	camera_x_angle_label.text = str("Camera X Angle: ", rotation_degrees.y)
+	camera_y_angle_label.text = str("Camera Y Angle: ", camera.rotation_degrees.x)
