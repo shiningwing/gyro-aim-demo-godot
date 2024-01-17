8788e86e5be46e1086e970abd7fbbf0e87a51399
Shining Wing <shiningwing@pm.me>
2024-01-17 19:36:14 -0400

    Create settings_item_toggle

---
+++ scenes/settings_item_toggle.gd
@@ -0,0 +1,31 @@
+extends HBoxContainer
+
+
+@export var label: String = "Setting Label"
+@export var config_section: String = "SectionName"
+@export var config_key: String = "key_name"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = label
+	if GameSettings.general[config_section][config_key]:
+		$CheckButton.button_pressed = true
+		$CheckButton.text = "On"
+	else:
+		$CheckButton.text = "Off"
+		$CheckButton.button_pressed = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_check_button_toggled(toggled_on):
+	if toggled_on:
+		GameSettings.general[config_section][config_key] = true
+		$CheckButton.text = "On"
+	else:
+		GameSettings.general[config_section][config_key] = false
+		$CheckButton.text = "Off"
