bc92820fe51bb333a7cc8cfd48a2822b2a61372a
Shining Wing <shiningwing@pm.me>
2024-01-17 06:43:01 -0400

    Add button to toggle debug in main menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,9 +1,14 @@
 extends Control
 
 
+@onready var toggle_debug_button: Button = (
+		$MainMenuPanel/VBoxContainer/HeaderContainer/Header/ToggleDebugButton)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if GameSettings.general["Debug"]["debug_mode"]:
+		toggle_debug_button.button_pressed = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +24,10 @@ func _on_game_start_button_pressed():
 func _on_save_settings_button_pressed():
 	GameSettings.write_general_config()
 	GameSettings.write_graphics_config()
+
+
+func _on_toggle_debug_button_toggled(toggled_on):
+	if toggled_on:
+		GameSettings.general["Debug"]["debug_mode"] = true
+	else:
+		GameSettings.general["Debug"]["debug_mode"] = false
