a0ca03f362efc849b46c56bb1e5135027434d5eb
Shining Wing <shiningwing@pm.me>
2024-01-24 18:30:24 -0400

    Add target object

---
+++ scenes/target.gd
@@ -0,0 +1,25 @@
+extends Area3D
+
+
+var destroyed := false
+var played_sound := false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if destroyed:
+		visible = false
+		if not played_sound:
+			$BreakSound.play()
+			played_sound = true
+		if played_sound and not $BreakSound.playing:
+			queue_free()
+
+
+func destroy():
+	destroyed = true
