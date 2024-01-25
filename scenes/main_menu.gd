1a15621b61c3f3fc0c71c97db68ed80f6a41fdd9
Shining Wing <shiningwing@pm.me>
2024-01-24 22:24:40 -0400

    Add help menu and main menu button for it

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -14,12 +14,6 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
-	if !MotionInput.calibration_timer_running:
-		calibrate_gyro_button.text = "Calibrate Gyro"
-		calibrate_gyro_button.disabled = false
-	if $CalibrationDialogContainer.visible:
-		if $CalibrationDialogContainer/CalibrationDialog.visible == false:
-			$CalibrationDialogContainer.visible = false
 
 
 func _on_game_start_button_pressed():
@@ -30,7 +24,5 @@ func _on_save_settings_button_pressed():
 	GameSettings.write_general_config()
 	GameSettings.write_graphics_config()
 
-
-func _on_calibrate_gyro_button_pressed():
-	$CalibrationDialogContainer.visible = true
-	$CalibrationDialogContainer/CalibrationDialog.visible = true
+func _on_help_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/help_menu.tscn")
