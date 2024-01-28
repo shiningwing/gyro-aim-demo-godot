2336dd53892f95f08ff0fd9024c622114d4c32bf
Shining Wing <shiningwing@pm.me>
2024-01-28 18:20:40 -0400

    Remove stray reference to removed main menu button

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,10 +1,6 @@
 extends Control
 
 
-@onready var calibrate_gyro_button: Button = (
-		$MainMenuPanel/VBoxContainer/HeaderContainer/Header/CalibrateGyroButton)
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 		pass
